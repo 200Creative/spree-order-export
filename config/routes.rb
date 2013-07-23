96cdcf280e9e7398d699bf5b27878b0907c25b16
Francis Li <mail@francisli.com>
2013-07-23 08:38:24 -0700

    Patching routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Add your extension routes here
-  namespace :admin do
-    resources :reports do
-      get 'order_export', :on => :collection
+  Spree::Core::Engine.routes.prepend do
+    namespace :admin do
+      resources :reports do
+        get 'order_export', :on => :collection
+      end
     end
   end
 end
-
