82b9b087df9acf7bbeb72aa700fd15327a4aa3e9
Francis Li <mail@francisli.com>
2013-07-23 08:46:33 -0700

    Replace searchlogic with ransack search

--- lib/order_export/reports_controller_ext.rb
+++ lib/order_export/reports_controller_ext.rb
@@ -24,7 +24,7 @@ module OrderExport
 
           params[:search][:order] ||= "descend_by_created_at"
 
-          @search = Spree::Order.searchlogic(params[:search])
+          @search = Spree::Order.search(params[:search])
 
           render and return unless export
 
