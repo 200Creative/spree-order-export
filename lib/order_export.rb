ed2ef775027f39a6c6467c2cb397bd523d9e7c63
Francis Li <mail@francisli.com>
2013-07-23 09:32:15 -0700

    Try to fix broken translations on report names

--- lib/order_export.rb
+++ lib/order_export.rb
@@ -10,7 +10,12 @@ module OrderExport
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
-
+      
+      # fix broken translation on core reports
+      I18n.load_path << Spree::OrderExport::Engine.config.paths["config/locales"].first
+      I18n.locale = I18n.default_locale
+      I18n.reload!
+  
       Spree::Admin::ReportsController::AVAILABLE_REPORTS.merge!({:order_export => {:name => "Order Export", :description => "Order Export Description"}})
       Spree::Admin::ReportsController.send(:require, RUBY_VERSION.split('.')[1].to_i > 8 ? 'csv' : 'fastercsv')
       Spree::Admin::ReportsController.send(:include, OrderExport::ReportsControllerExt)
